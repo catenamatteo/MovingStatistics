a8cbf1a98839172b4cae5b8ac114a3decfdc30e8
Matteo Catena <matteo.catena@isti.cnr.it>
2018-02-27 15:18:25 +0100

    first commit

---
+++ src/eu/nicecode/stats/MovingStatistics.java
@@ -0,0 +1,160 @@
+package eu.nicecode.stats;
+
+import java.util.Collections;
+import java.util.concurrent.TimeUnit;
+
+import it.unimi.dsi.fastutil.doubles.DoubleArrayList;
+import it.unimi.dsi.fastutil.doubles.DoubleList;
+import it.unimi.dsi.fastutil.longs.Long2ObjectAVLTreeMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectSortedMap;
+
+/**
+ * Compute statistics over a moving (rolling) time window.
+ *
+ * @author Matteo Catena
+ *
+ */
+public class MovingStatistics {
+
+	private Long2ObjectSortedMap<DoubleList> time2values;
+	private DoubleList values;
+	final private long nanoTimeOffset;
+	private long timeWindow;
+	private TimeUnit timeUnit;
+
+	private double sum = 0;
+
+	/**
+	 * It computes sum, average, median, and percentile(s) statistics over a
+	 * moving (rolling) time window.
+	 * 
+	 * @param timeWindow
+	 */
+	public MovingStatistics(long timeWindow, TimeUnit timeUnit) {
+
+		time2values = new Long2ObjectAVLTreeMap<>();
+		values = new DoubleArrayList();
+		nanoTimeOffset = System.nanoTime();
+		this.timeWindow = timeWindow;
+		this.timeUnit = timeUnit;
+	}
+
+	private long updateTime() {
+
+		long nanoCallTime = System.nanoTime();
+		long callTime = timeUnit.convert(nanoCallTime - nanoTimeOffset, TimeUnit.NANOSECONDS);
+
+		if (!time2values.isEmpty() && callTime - time2values.firstLongKey() > timeWindow) {
+
+			// discard older values
+			time2values = new Long2ObjectAVLTreeMap<>(time2values.tailMap(callTime - timeWindow));
+			values.clear();
+			sum = 0;
+
+			// recreate the sorted value list
+			for (DoubleList l : time2values.values())
+				for (double d : l) {
+					values.add(d);
+					sum += d;
+				}
+			Collections.sort(values);
+		}
+
+		return callTime;
+	}
+
+	/**
+	 * Add a new value. The insertion time is given by System.nanoTime() w.r.t.
+	 * the constructor call time.
+	 * 
+	 * @param val
+	 */
+	public void add(double val) {
+
+		long callTime = updateTime();
+
+		if (!time2values.containsKey(callTime))
+			time2values.put(callTime, new DoubleArrayList());
+		time2values.get(callTime).add(val);
+		int idx = Collections.binarySearch(values, val);
+		if (idx < 0)
+			idx = -idx - 1;
+		values.add(idx, val);
+		sum += val;
+	}
+
+	/**
+	 * Get the index-th percentile over the time window. E.g., for the
+	 * 99.9th-tile, .getPercentile(99.9)
+	 * 
+	 * @param index
+	 * @return
+	 */
+	public double getPercentile(double index) {
+
+		updateTime();
+
+		if (values.isEmpty()) {
+
+			return 0;
+
+		} else {
+
+			int idx = ((int) Math.ceil((index / 100.0) * values.size())) - 1;
+			return values.getDouble(idx);
+
+		}
+	}
+
+	/**
+	 * Get the median value over the time window.
+	 * 
+	 * @return
+	 */
+	public double getMedian() {
+
+		updateTime();
+
+		if (values.isEmpty()) {
+
+			return 0;
+
+		} else {
+
+			double median = 0.0;
+
+			if (values.size() % 2 == 0)
+				median = (values.getDouble(values.size() / 2) + values.getDouble(values.size() / 2 - 1)) / 2;
+			else
+				median = values.getDouble(values.size() / 2);
+
+			return median;
+		}
+	}
+
+	public double getSum() {
+
+		updateTime();
+		
+		return sum;
+	}
+
+	/**
+	 * Return the average value over the time window.
+	 * 
+	 * @return
+	 */
+	public double getAverage() {
+
+		updateTime();
+
+		if (values.isEmpty()) {
+
+			return 0;
+
+		} else {
+
+			return sum / values.size();
+		}
+	}
+}
---
+++ test/MovingStatisticsTest.java
@@ -0,0 +1,218 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.Test;
+
+import eu.nicecode.stats.MovingStatistics;
+
+public class MovingStatisticsTest {
+
+	static double getPercentile(List<Double> values, double index) {
+
+
+		if (values.isEmpty()) {
+
+			return 0;
+
+		} else {
+
+			int idx = ((int) Math.ceil((index / 100.0) * values.size())) - 1;
+			return values.get(idx);
+
+		}
+	}
+
+	static double getMedian(List<Double> values) {
+
+
+		if (values.isEmpty()) {
+
+			return 0;
+
+		} else {
+
+			double median = 0.0;
+
+			if (values.size() % 2 == 0)
+				median = (values.get(values.size() / 2) + values.get(values.size() / 2 - 1)) / 2;
+			else
+				median = values.get(values.size() / 2);
+
+			return median;
+		}
+	}
+
+	static double getSum(List<Double> values) {
+
+		double sum = 0;
+		for (double d : values) sum+=d;
+		return sum;
+	}
+
+	static double getAverage(List<Double> values) {
+
+
+		if (values.isEmpty()) {
+
+			return 0;
+
+		} else {
+
+			return getSum(values) / values.size();
+		}
+	}
+	
+	@Test
+	public void basicTest() {
+		
+		List<Double> list = new ArrayList<>();
+		MovingStatistics ms = new MovingStatistics(5, TimeUnit.SECONDS);
+		for (int i = 0; i < 101; i++) {
+			ms.add(i);
+			list.add((double) i);
+		}
+		
+		assertEquals(getSum(list), ms.getSum(), 0);
+		assertEquals(getAverage(list), ms.getAverage(), 0);
+		assertEquals(getMedian(list), ms.getMedian(), 0);
+		assertEquals(getPercentile(list, 90), ms.getPercentile(90), 0);		
+		
+	}
+	
+	@Test
+	public void timedTest() {
+				
+		try {
+			
+			List<Double> list1 = new ArrayList<>();
+			MovingStatistics ms = new MovingStatistics(5, TimeUnit.SECONDS);
+			for (int i = 0; i < 101; i++) {
+				ms.add(i);
+				list1.add((double)i);
+			}
+			
+			List<Double> list2 = new ArrayList<>();
+			Thread.sleep(TimeUnit.SECONDS.toMillis(4));
+			
+			for (int i = 101; i < 201; i++) {
+				ms.add(i);
+				list1.add((double)i);
+				list2.add((double)i);
+			}
+			assertEquals(getSum(list1), ms.getSum(), 0);
+			assertEquals(getAverage(list1), ms.getAverage(), 0);
+			assertEquals(getMedian(list1), ms.getMedian(), 0);
+			assertEquals(getPercentile(list1,90), ms.getPercentile(90), 0);
+			
+			//some values are going to expire (out of window)
+			Thread.sleep(TimeUnit.SECONDS.toMillis(2));
+			
+			assertEquals(getSum(list2), ms.getSum(), 0);
+			assertEquals(getAverage(list2), ms.getAverage(), 0);
+			assertEquals(getMedian(list2), ms.getMedian(), 0);
+			assertEquals(getPercentile(list2,90), ms.getPercentile(90), 0);
+			
+			//reinsert the values and test
+			Thread.sleep(TimeUnit.SECONDS.toMillis(1));
+			for (int i = 0; i < 101; i++) {
+				ms.add(i);
+				list2.add((double)i);
+			}
+			Collections.sort(list2);
+			assertEquals(getSum(list2), ms.getSum(), 0);
+			assertEquals(getAverage(list2), ms.getAverage(), 0);
+			assertEquals(getMedian(list2), ms.getMedian(), 0);
+			assertEquals(getPercentile(list2,90), ms.getPercentile(90), 0);
+			
+
+			
+		} catch (InterruptedException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}	
+
+	@Test
+	public void expirationTest() {
+				
+		try {
+			
+			MovingStatistics ms = new MovingStatistics(2, TimeUnit.SECONDS);
+			for (int i = 0; i < 101; i++) {
+				ms.add(i);
+			}
+			
+			Thread.sleep(TimeUnit.SECONDS.toMillis(3));
+			
+			assertEquals(0, ms.getSum(), 0);
+			assertEquals(0, ms.getAverage(), 0);
+			assertEquals(0, ms.getMedian(), 0);
+			assertEquals(0, ms.getPercentile(90), 0);
+			
+		} catch (InterruptedException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}
+	
+	@Test
+	public void testAnotherTimeUnit() {
+				
+		try {
+			
+			List<Double> list1 = new ArrayList<>();
+			MovingStatistics ms = new MovingStatistics(500, TimeUnit.MILLISECONDS);
+			for (int i = 0; i < 101; i++) {
+				ms.add(i);
+				list1.add((double)i);
+			}
+			
+			List<Double> list2 = new ArrayList<>();
+			Thread.sleep(400);
+			
+			for (int i = 101; i < 201; i++) {
+				ms.add(i);
+				list1.add((double)i);
+				list2.add((double)i);
+			}
+			assertEquals(getSum(list1), ms.getSum(), 0);
+			assertEquals(getAverage(list1), ms.getAverage(), 0);
+			assertEquals(getMedian(list1), ms.getMedian(), 0);
+			assertEquals(getPercentile(list1,90), ms.getPercentile(90), 0);
+			
+			//some values are going to expire (out of window)
+			Thread.sleep(200);
+			
+			assertEquals(getSum(list2), ms.getSum(), 0);
+			assertEquals(getAverage(list2), ms.getAverage(), 0);
+			assertEquals(getMedian(list2), ms.getMedian(), 0);
+			assertEquals(getPercentile(list2,90), ms.getPercentile(90), 0);
+			
+			//reinsert the values and test
+			Thread.sleep(100);
+			for (int i = 0; i < 101; i++) {
+				ms.add(i);
+				list2.add((double)i);
+			}
+			Collections.sort(list2);
+			assertEquals(getSum(list2), ms.getSum(), 0);
+			assertEquals(getAverage(list2), ms.getAverage(), 0);
+			assertEquals(getMedian(list2), ms.getMedian(), 0);
+			assertEquals(getPercentile(list2,90), ms.getPercentile(90), 0);
+			
+
+			
+		} catch (InterruptedException e) {
+			
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+	}	
+	
+}
